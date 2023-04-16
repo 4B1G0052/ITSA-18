cb9529a145ec58714646ba5c6ff6583dc62317dd
4B1G0052 <4B1G0052@stust.edu.tw>
2023-04-16 20:11:27 +0800

    加入專案檔案。

---
+++ ITSA 18/ITSA 18.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{	//a的宣告裡面以行左至右排列，尾巴的字元就多一次
+	string a = "~!@#$%^&*()_++`1234567890-=={}||qwertyuiop[]\\:""asdffghjkl;''<>??zxcvbnm,.//";
+	int a_len = a.size();
+
+	string input;
+	string output[127];
+	getline(cin, input);//可以讀spacebar
+	transform(input.begin(), input.end(), input.begin(), ::tolower);//::tolower不然會編譯失敗
+	int input_len = input.size();
+
+	for (int i = 0; i < input_len; i++) {//input長度為外迴圈容量
+
+		for (int j = 0; j < a_len; j++) {//鍵盤長度
+
+			if (input[i] == ' ') {//空白就等於空白
+
+				output[i] = ' ';
+				break;
+			}
+			else if (input[i] == a[j]) {//輸入比鍵盤
+				output[i] = a[j + 1];//一樣就+1
+				break;
+			};
+		};
+		cout << output[i];
+	};
+
+	cout << endl;
+	return 0;
+}
